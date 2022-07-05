b6daffb5bc249058ebe890e7e2a078fc6fb152c0
Franz Mejia <mejiafranz@gmail.com>
2022-07-05 00:16:17 -0400

    Add some simple queries

--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -1,7 +1,27 @@
 package com.packt.cardatabase.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface CarRepository extends CrudRepository<Car, Long>{
   
+  // Fetch data by brand
+  List<Car> findByBrand(String brand);
+
+  // Fetch data by color
+  List<Car> findByColor(String color);
+
+  // Fetch data by year
+  List<Car> findByYears(int years);
+  
+  // Fetch data by brand and model
+  List<Car> findByBrandAndModel(String brand, String model);
+
+  // Fetch data by brand or color
+  List<Car> findByBrandOrColor(String brand, String color);
+
+  // Fetch cars by brand and sort by year
+  List<Car> findByBrandOrderByYearsAsc(String brand);
+
 }

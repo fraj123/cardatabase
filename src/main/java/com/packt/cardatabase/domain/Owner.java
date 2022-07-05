47322c695a23a6a10a8360ee456692e9d1268a2b
Franz Mejia <mejiafranz@gmail.com>
2022-07-05 00:34:36 -0400

    Add Owner Model and Repository

---
+++ src/main/java/com/packt/cardatabase/domain/Owner.java
@@ -0,0 +1,50 @@
+package com.packt.cardatabase.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Owner {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long ownerid;
+
+  private String firstname;
+
+  private String lastname;
+
+  public Owner() {  }
+
+  public Owner(String firstname, String lastname) {
+    super();
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+
+  public long getOwnerid() {
+      return ownerid;
+  }
+
+  public void setOwnerid(long ownerid) {
+      this.ownerid = ownerid;
+  }
+
+  public String getLastname() {
+      return lastname;
+  }
+
+  public void setLastname(String lastname) {
+      this.lastname = lastname;
+  }
+
+  public String getFirstname() {
+      return firstname;
+  }
+
+  public void setFirstname(String firstname) {
+      this.firstname = firstname;
+  }
+}

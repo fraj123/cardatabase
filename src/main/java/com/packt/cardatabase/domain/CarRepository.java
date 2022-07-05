052910808b4638b3ef875672b25b9b317f566c83
Franz Mejia <mejiafranz@gmail.com>
2022-07-05 00:22:11 -0400

    Add more complet SQL queries

--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -2,12 +2,18 @@ package com.packt.cardatabase.domain;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CarRepository extends CrudRepository<Car, Long>{
   
-  // Fetch data by brand
+  // Fetch Cars by brand using SQL
+  @Query("select c from Car c where c.brand = ?1")
   List<Car> findByBrand(String brand);
+
+  // More complex queries
+  @Query("select c from Car c where c.brand like %?1")
+  List<Car> findByBrandEndsWith(String brand);
 
   // Fetch data by color
   List<Car> findByColor(String color);
